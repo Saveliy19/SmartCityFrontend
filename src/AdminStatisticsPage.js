d34223af2851fb9bcae7bc5f11f76fc9488b3255
Saveliy19 <saveliy200319@gmail.com>
2024-05-17 20:26:13 +0300

    Fixed statistics

--- src/AdminStatisticsPage.js
+++ src/AdminStatisticsPage.js
@@ -1,10 +1,10 @@
-import './AdminStatisticsPage.css'
+import './AdminStatisticsPage.css';
 import Header from './Header';
 import React, { useState, useEffect } from 'react';
 import { Link, useParams } from 'react-router-dom';
 import axios from 'axios';
-import Chart from 'chart.js/auto';
 import { Bar, Line } from 'react-chartjs-2';
+import Chart from 'chart.js/auto';
 
 const AnalyticsPage = () => {
   const { regionName, cityName } = useParams();
@@ -14,10 +14,6 @@ const AnalyticsPage = () => {
   const [analyticsData, setAnalyticsData] = useState(null);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
-  const [complaintChart, setComplaintChart] = useState(null);
-  const [complaintChartRegion, setComplaintChartRegion] = useState(null);
-  const [initiativeChart, setInitiativeChart] = useState(null);
-  const [initiativeChartRegion, setInitiativeChartRegion] = useState(null);
 
   const [complaintsPerDay, setComplaintsPerDay] = useState({});
   const [initiativesPerDay, setInitiativesPerDay] = useState({});
@@ -40,7 +36,6 @@ const AnalyticsPage = () => {
         }
       );
       setAnalyticsData(response.data[0]);
-
     } catch (error) {
       setError(error);
     }
@@ -48,46 +43,18 @@ const AnalyticsPage = () => {
   };
 
   useEffect(() => {
-    // Проверяем, есть ли данные для аналитики
     if (analyticsData) {
-      // Удаление предыдущего графика, если он был
-      if (complaintChart) {
-        complaintChart.clear();
-        complaintChart.destroy();
-      }
-
-      if (complaintChartRegion) {
-        complaintChartRegion.clear();
-        complaintChartRegion.destroy();
-      }
-
-      if (initiativeChart) {
-        initiativeChart.clear();
-        initiativeChart.destroy();
-      }
-
-      if (initiativeChartRegion) {
-        initiativeChartRegion.clear();
-        initiativeChartRegion.destroy();
-      }
-      
-
       const categoriesCity = Object.keys(analyticsData.count_per_category_city);
       const countDataCity = Object.values(analyticsData.count_per_category_city);
       const categoriesRegion = Object.keys(analyticsData.count_per_category_region);
       const countDataRegion = Object.values(analyticsData.count_per_category_region);
-
-      const initiativeCategoriesCity = Object.keys(analyticsData.count_per_category_city);
-      const initiativeCountDataCity = Object.values(analyticsData.count_per_category_city);
-      const initiativeCategoriesRegion = Object.keys(analyticsData.count_per_category_region);
-      const initiativeCountDataRegion = Object.values(analyticsData.count_per_category_region);
 
       const updatedChartDataCity = {
         labels: categoriesCity,
         datasets: [
           {
             data: countDataCity,
-            label: "Количество",
+            label: "Количество жалоб в городе",
             borderColor: "#3333ff",
             backgroundColor: "rgba(0, 0, 255, 0.5)",
             fill: true
@@ -95,27 +62,32 @@ const AnalyticsPage = () => {
         ]
       };
 
-      const updatedInitiativeChartDataCity = {
-        labels: initiativeCategoriesCity,
-        datasets: [
-          {
-            data: initiativeCountDataCity,
-            label: "Количество",
-            borderColor: "#3333ff",
-            backgroundColor: "rgba(0, 0, 255, 0.5)",
-            fill: true
-          }
-        ]
-      };
-      
       const updatedChartDataRegion = {
         labels: categoriesRegion,
         datasets: [
           {
             data: countDataRegion,
-            label: "Количество",
+            label: "Количество жалоб в регионе",
             borderColor: "#ff3333",
             backgroundColor: "rgba(255, 0, 0, 0.5)",
+            fill: true
+          }
+        ]
+      };
+
+      const initiativeCategoriesCity = Object.keys(analyticsData.init_count_per_category_city);
+      const initiativeCountDataCity = Object.values(analyticsData.init_count_per_category_city);
+      const initiativeCategoriesRegion = Object.keys(analyticsData.init_count_per_category_region);
+      const initiativeCountDataRegion = Object.values(analyticsData.init_count_per_category_region);
+
+      const updatedInitiativeChartDataCity = {
+        labels: initiativeCategoriesCity,
+        datasets: [
+          {
+            data: initiativeCountDataCity,
+            label: "Количество инициатив в городе",
+            borderColor: "#3333ff",
+            backgroundColor: "rgba(0, 0, 255, 0.5)",
             fill: true
           }
         ]
@@ -126,7 +98,7 @@ const AnalyticsPage = () => {
         datasets: [
           {
             data: initiativeCountDataRegion,
-            label: "Количество",
+            label: "Количество инициатив в регионе",
             borderColor: "#ff3333",
             backgroundColor: "rgba(255, 0, 0, 0.5)",
             fill: true
@@ -134,237 +106,75 @@ const AnalyticsPage = () => {
         ]
       };
 
-      // Создание нового графика
-  const ctx = document.getElementById('complaintChart');
-  const newChart = new Chart(ctx, {
-    type: 'bar',
-    data: updatedChartDataCity,
-    options: {
-      // Заголовок графика
-      plugins: {
-        title: {
-          display: true,
-          text: 'Количество жалоб на категорию в городе',
-          font: {
-            size: 10,
-            weight: 'bold'
-          }
-        }
-      },
-      // Подпись оси X
-      scales: {
-        x: {
-          title: {
-            display: true,
-            text: 'Категория'
-          }
-        },
-        // Подпись оси Y
-        y: {
-          title: {
-            display: true,
-            text: 'Количество'
-          }
-        }
-      },
-      // Ваши другие настройки графика
-      responsive: true,
-      maintainAspectRatio: false, // Изменение размера графика без сохранения пропорций
-      width: 400, // Устанавливаем новую ширину графика
-      height: 300, // Устанавливаем новую высоту графика
-    }
-  });
-  setComplaintChart(newChart);
+      setComplaintChartData(updatedChartDataCity);
+      setInitiativeChartData(updatedInitiativeChartDataCity);
+      setComplaintChartRegionData(updatedChartDataRegion);
+      setInitiativeChartRegionData(updatedInitiativeChartDataRegion);
 
-  const ctxInitiative = document.getElementById('initiativeChart');
-  const newInitiativeChart = new Chart(ctxInitiative, {
-    type: 'bar',
-    data: updatedInitiativeChartDataCity,
-    options: {
-      // Заголовок графика
-      plugins: {
-        title: {
-          display: true,
-          text: 'Количество инициатив на категорию в городе',
-          font: {
-            size: 10,
-            weight: 'bold'
-          }
-        }
-      },
-      // Подпись оси X
-      scales: {
-        x: {
-          title: {
-            display: true,
-            text: 'Категория'
-          }
-        },
-        // Подпись оси Y
-        y: {
-          title: {
-            display: true,
-            text: 'Количество'
-          }
-        }
-      },
-      // Ваши другие настройки графика
-      responsive: true,
-      maintainAspectRatio: false, // Изменение размера графика без сохранения пропорций
-      width: 400, // Устанавливаем новую ширину графика
-      height: 300, // Устанавливаем новую высоту графика
+      setComplaintsPerDay(analyticsData.comp_per_day || {});
+      setInitiativesPerDay(analyticsData.init_per_day || {});
     }
-  });
-  setInitiativeChart(newInitiativeChart);
+  }, [analyticsData]);
 
-  // Создание графика для региона
-  const ctxRegion = document.getElementById('complaintChartRegion');
-  const newChartRegion = new Chart(ctxRegion, {
-    type: 'bar',
-    data: updatedChartDataRegion,
-    options: {
-      // Заголовок графика
-      plugins: {
-        title: {
-          display: true,
-          text: 'Количество жалоб на категорию в регионе',
-          font: {
-            size: 10,
-            weight: 'bold'
-          }
-        }
+  const combinedChartData = {
+    labels: Object.keys(complaintsPerDay),
+    datasets: [
+      {
+        label: 'Жалобы',
+        data: Object.values(complaintsPerDay),
+        fill: false,
+        borderColor: 'rgb(255, 99, 132)',
+        tension: 0.1
       },
-      // Подпись оси X
-      scales: {
-        x: {
-          title: {
-            display: true,
-            text: 'Категория'
-          }
-        },
-        // Подпись оси Y
-        y: {
-          title: {
-            display: true,
-            text: 'Количество'
-          }
-        }
-      },
-      // Ваши другие настройки графика
-      responsive: true,
-      maintainAspectRatio: false, // Изменение размера графика без сохранения пропорций
-      width: 400, // Устанавливаем новую ширину графика
-      height: 300, // Устанавливаем новую высоту графика
-    }
-  });
-  setComplaintChartRegion(newChartRegion);
-
-  const ctxInitiativeRegion = document.getElementById('initiativeChartRegion');
-  const newInitiativeChartRegion = new Chart(ctxInitiativeRegion, {
-    type: 'bar',
-    data: updatedInitiativeChartDataRegion,
-    options: {
-      // Заголовок графика
-      plugins: {
-        title: {
-          display: true,
-          text: 'Количество инициатив на категорию в регионе',
-          font: {
-            size: 10,
-            weight: 'bold'
-          }
-        }
-      },
-      // Подпись оси X
-      scales: {
-        x: {
-          title: {
-            display: true,
-            text: 'Категория'
-          }
-        },
-        // Подпись оси Y
-        y: {
-          title: {
-            display: true,
-            text: 'Количество'
-          }
-        }
-      },
-      // Ваши другие настройки графика
-      responsive: true,
-      maintainAspectRatio: false, // Изменение размера графика без сохранения пропорций
-      width: 400, // Устанавливаем новую ширину графика
-      height: 300, // Устанавливаем новую высоту графика
-    }
-  });
-  setInitiativeChartRegion(newInitiativeChartRegion);
-  // Обновление данных о количестве заявок в день
-  setComplaintsPerDay(analyticsData.comp_per_day || {});
-  setInitiativesPerDay(analyticsData.init_per_day || {});
-
+      {
+        label: 'Инициативы',
+        data: Object.values(initiativesPerDay),
+        fill: false,
+        borderColor: 'rgb(54, 162, 235)',
+        tension: 0.1
       }
-    }, [analyticsData]);
-
-
-    // Добавляем функцию для преобразования данных о количестве заявок в день в формат, подходящий для графика
-    const combinedChartData = {
-      labels: Object.keys(complaintsPerDay),
-      datasets: [
-          {
-              label: 'Жалобы',
-              data: Object.values(complaintsPerDay),
-              fill: false,
-              borderColor: 'rgb(255, 99, 132)',
-              tension: 0.1
-          },
-          {
-              label: 'Инициативы',
-              data: Object.values(initiativesPerDay),
-              fill: false,
-              borderColor: 'rgb(54, 162, 235)',
-              tension: 0.1
-          }
-      ]
+    ]
   };
 
   const chartOptions = {
     responsive: true,
-    maintainAspectRatio: false, // Устанавливаем значение false, чтобы можно было задать ширину и высоту графика в пикселях
-    width: 400, // Устанавливаем новую ширину графика
-    height: 300 // Устанавливаем новую высоту графика
-};
+    maintainAspectRatio: false,
+    width: 400,
+    height: 300
+  };
 
+  const [complaintChartData, setComplaintChartData] = useState({});
+  const [initiativeChartData, setInitiativeChartData] = useState({});
+  const [complaintChartRegionData, setComplaintChartRegionData] = useState({});
+  const [initiativeChartRegionData, setInitiativeChartRegionData] = useState({});
 
   return (
     <div>
       <Header />
-      
-  
       <div className='param-container'>
         <form onSubmit={handleSubmit}>
-            <label htmlFor="startTime">Начало периода: </label>
-            <input
-                type="datetime-local"
-                id="startTime"
-                value={startTime}
-                onChange={(e) => setStartTime(e.target.value)}
-            />
-            <label htmlFor="endTime">Конец периода: </label>
-            <input
-                type="datetime-local"
-                id="endTime"
-                value={endTime}
-                onChange={(e) => setEndTime(e.target.value)}
-            />
-            <label htmlFor="rowsCount">Количество наиболее популярных петиций: </label>
-            <input
-                type="number"
-                id="rowsCount"
-                value={rowsCount}
-                onChange={(e) => setRowsCount(parseInt(e.target.value))}
-            />
-            <button type="submit" disabled={loading}>Получить детальный отчет</button>
+          <label htmlFor="startTime">Начало периода: </label>
+          <input
+            type="datetime-local"
+            id="startTime"
+            value={startTime}
+            onChange={(e) => setStartTime(e.target.value)}
+          />
+          <label htmlFor="endTime">Конец периода: </label>
+          <input
+            type="datetime-local"
+            id="endTime"
+            value={endTime}
+            onChange={(e) => setEndTime(e.target.value)}
+          />
+          <label htmlFor="rowsCount">Количество наиболее популярных петиций: </label>
+          <input
+            type="number"
+            id="rowsCount"
+            value={rowsCount}
+            onChange={(e) => setRowsCount(parseInt(e.target.value))}
+          />
+          <button type="submit" disabled={loading}>Получить детальный отчет</button>
         </form>
       </div>
       {loading && <p>Loading...</p>}
@@ -372,68 +182,66 @@ const AnalyticsPage = () => {
 
       {analyticsData && (
         <div className='main-info-container'>
-              <div>
-            <h2>Статистика в городе {cityName}</h2>
-            
+          <h2>Статистика в городе {cityName}</h2>
           <div className='chart-container'>
-              <div className='line-chart'>
-                  <Line data={combinedChartData} options={chartOptions} />
-              </div>
+            <div className='line-chart'>
+              <Line data={combinedChartData} options={chartOptions} />
+            </div>
           </div>
           <div className='hist-container'>
-            <div className='hist'>
-              <canvas id="complaintChartRegion"></canvas>
-            </div>
-            <div className='hist'>
-              <canvas id="complaintChart"></canvas>
-            </div>
-            <div className='hist'>
-              <canvas id="initiativeChartRegion"></canvas>
-            </div>
-            <div className='hist'>
-              <canvas id="initiativeChart"></canvas>
-            </div>
+            {complaintChartRegionData.labels && (
+              <div className='hist'>
+                <Bar data={complaintChartRegionData} options={chartOptions} />
+              </div>
+            )}
+            {complaintChartData.labels && (
+              <div className='hist'>
+                <Bar data={complaintChartData} options={chartOptions} />
+              </div>
+            )}
+            {initiativeChartRegionData.labels && (
+              <div className='hist'>
+                <Bar data={initiativeChartRegionData} options={chartOptions} />
+              </div>
+            )}
+            {initiativeChartData.labels && (
+              <div className='hist'>
+                <Bar data={initiativeChartData} options={chartOptions} />
+              </div>
+            )}
           </div>
-          
-          
           <div className='most-popular-petitions-container'>
             <div className='petition-list-container'>
-              <h3>Наиболее популярные жалобы в городе {cityName}: {rowsCount} записей</h3>
+              <h3>Наиболее популярные жалобы в городе {cityName}: {rowsCount}</h3>
               <div className='petitions-container'>
-              {analyticsData.most_popular_city_complaints.map(complaint => (
-                <div className='info' key={complaint.id}>
-                  <p><Link to={`/petition/${complaint.id}`}>ID: {complaint.id} </Link> - {complaint.header}</p>
-                  <p>Категория: <strong>{complaint.category}</strong></p>
-                  <p>Дата подачи: <strong>{complaint.submission_time}</strong></p>
-                  <p>Количество подписей: <strong>{complaint.like_count}</strong></p>
-                </div>
+                {analyticsData.most_popular_city_complaints.map(complaint => (
+                  <div className='info' key={complaint.id}>
+                    <p><Link to={`/petition/${complaint.id}`}>ID: {complaint.id} </Link> - {complaint.header}</p>
+                    <p>Категория: <strong>{complaint.category}</strong></p>
+                    <p>Дата подачи: <strong>{complaint.submission_time}</strong></p>
+                    <p>Количество подписей: <strong>{complaint.like_count}</strong></p>
+                  </div>
                 ))}
               </div>
             </div>
-
             <div className='petition-list-container'>
-              <h3>Наиболее популярные инициативы в городе {cityName}: {rowsCount} записей</h3>
+              <h3>Наиболее популярные инициативы в городе {cityName}: {rowsCount}</h3>
               <div className='petitions-container'>
                 {analyticsData.most_popular_city_initiatives.map(initiative => (
                   <div className='info' key={initiative.id}>
                     <p><Link to={`/petition/${initiative.id}`}>ID: {initiative.id}</Link> - {initiative.header}</p>
                     <p>Категория: <strong>{initiative.category}</strong></p>
-                  <p>Дата подачи: <strong>{initiative.submission_time}</strong></p>
-                  <p>Количество подписей: <strong>{initiative.like_count}</strong></p>
+                    <p>Дата подачи: <strong>{initiative.submission_time}</strong></p>
+                    <p>Количество подписей: <strong>{initiative.like_count}</strong></p>
                   </div>
                 ))}
               </div> 
             </div>
           </div>
         </div>
-        </div>
-        
-        )}
+      )}
     </div>
   );
-
-  
-
 };
 
 export default AnalyticsPage;
