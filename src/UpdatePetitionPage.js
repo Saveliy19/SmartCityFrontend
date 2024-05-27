abda71f9f5f5a52d7fc34becfc8f4bb683d5d709
Saveliy19 <saveliy200319@gmail.com>
2024-05-27 18:46:22 +0300

    fixed bugs

--- src/UpdatePetitionPage.js
+++ src/UpdatePetitionPage.js
@@ -3,12 +3,14 @@ import './UpdatePetitionPage.css';
 import Header from './Header'; // Импорт компонента Header
 import { useNavigate, useParams } from 'react-router-dom';
 import axios from 'axios';
+import { Oval } from 'react-loader-spinner'; // Импорт компонента Oval из react-loader-spinner
 
 function PetitionStatusForm({ onUpdateStatus, onCancel }) {
   const [status, setStatus] = useState('на модерации');
   const [comment, setComment] = useState('');
   const [successMessage, setSuccessMessage] = useState('');
   const [errorMessage, setErrorMessage] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
   const { petitionId } = useParams();
   const navigate = useNavigate();
 
@@ -36,18 +38,24 @@ function PetitionStatusForm({ onUpdateStatus, onCancel }) {
       comment: ('Новый статус: ' + status + '. Комментарий администратора: ' + comment)
     };
 
+    setIsLoading(true); // Установить состояние загрузки в true перед запросом
+
     // Отправляем данные на сервер
     axios.post('http://127.0.0.1:8000/update_petition', data)
       .then(response => {
         // Обработка успешного ответа от сервера
+        setIsLoading(false); // Установить состояние загрузки в false после получения ответа
         setSuccessMessage('Статус заявки успешно обновлен.');
         setErrorMessage(null); // Очистка сообщения об ошибке
         onUpdateStatus(status, comment);
       })
       .catch(error => {
         // Обработка ошибки при запросе
+        setIsLoading(false); // Установить состояние загрузки в false при ошибке
         if (error.response) {
-          if (error.response.status === 500) {
+          if (error.response.status === 403) {
+            setErrorMessage('У Вас нет прав на изменение этой заявки.');
+          } else if (error.response.status === 500) {
             setErrorMessage('Что-то пошло не так, попробуйте позже.');
           } else if (error.response.status === 401) {
             setErrorMessage('Необходима повторная авторизация. Пожалуйста, перезайдите в систему.');
@@ -69,8 +77,22 @@ function PetitionStatusForm({ onUpdateStatus, onCancel }) {
       <Header />
       
       <div className="petition-status-form">
-      {successMessage  && <div className="success">{successMessage}</div>}
-      {errorMessage && !successMessage && <div className="error-message">{errorMessage}</div>}
+        {successMessage && <div className="success-message">{successMessage}</div>}
+        {errorMessage && !successMessage && <div className="error-message">{errorMessage}</div>}
+        {isLoading && (
+          <div className="loading-indicator">
+            <Oval
+              height={50}
+              width={50}
+              color="#0080ff"
+              visible={true}
+              ariaLabel='oval-loading'
+              secondaryColor="#f0f0f0"
+              strokeWidth={2}
+              strokeWidthSecondary={2}
+            />
+          </div>
+        )} {/* Индикатор загрузки */}
         <p>Заявка № {petitionId}</p>
         <form onSubmit={handleSubmit}>
           <div className="form-group">
@@ -93,7 +115,7 @@ function PetitionStatusForm({ onUpdateStatus, onCancel }) {
             />
           </div>
           <div className="button-group">
-            <button type="submit" className="update-button">Обновить статус</button>
+            <button type="submit" className="update-button" disabled={isLoading}>Обновить статус</button>
             <button type="button" className="cancel-button" onClick={handleGoBack}>Назад</button>
           </div>
         </form>
